ea1a3326e695a790b5a8ac3b0ddc9b98bb2c74f4
Xavier Cambar <xcambar@gmail.com>
2013-09-11 18:44:06 +0200

    Added a simple demo

--- app/aura_components/coll_view_example/main.js
+++ app/aura_components/coll_view_example/main.js
@@ -1,12 +1,42 @@
-define({
-  generateData: function () {
-    var random = Math.random().toString().substring(2).split('');
-    return random.map(function (number) {
-      return { value: number };
-    });
-  },
-  initialize: function () {
-    var data = this.generateData();
-    /* To Be Continued */
-  }
-})
+(function () {
+
+  function collectionTemplate (renderItems) {
+    return "<ul>" +renderItems() + "</ul>";
+  };
+
+  function itemTemplate (item) {
+    return "<li>" + item.value + "</li>";
+  };
+
+
+
+  define({
+    generateData: function () {
+      var random = Math.random().toString().substring(2).split('');
+      console.log(random);
+      return random.map(function (number) {
+        return { value: number };
+      });
+    },
+    initialize: function () {
+      var data = this.generateData();
+      var coll = this.sandbox.CollectionView(data, {
+        el: this.$el,
+        template: collectionTemplate,
+          itemTemplate: itemTemplate
+      });
+      coll.render();
+
+      setTimeout(function () {
+        coll.appendItem({value: "Added"});
+      }, 1000);
+      setTimeout(function () {
+        coll.prependItem({value: "Added too"});
+      }, 2000);
+    }
+  });
+
+
+
+})();
+
