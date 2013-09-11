62f5bb471f1921e3315cc2b24051c1191564fe0c
Xavier Cambar <xcambar@gmail.com>
2013-09-11 18:41:34 +0200

    Enabled debugger in Aura

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 require(['bower_components/aura/lib/aura'], function (Aura) {
   "use strict";
-  Aura()
+  Aura({debug: {enable: true}})
     .use('../src/collection_view')
     .start({ components: 'body' }).then(function() {
       console.warn('Aura started...');
